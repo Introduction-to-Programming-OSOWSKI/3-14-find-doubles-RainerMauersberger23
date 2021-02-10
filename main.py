375ef8dac03ae583db238e705932f0ada01c5499
RainerMauersberger23 <829532@mystma.org>
2021-02-10 20:08:31 +0000

    FindDoubles

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def findDoubles(w):
+
+    for i in range(0, len(w)):
+        if w[i] == w[i + 1]:
+            return True
+        else:
+            return False
+print (findDoubles("nintendo"))
