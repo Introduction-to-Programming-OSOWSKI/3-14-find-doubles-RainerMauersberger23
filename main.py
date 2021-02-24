2459e9364740ecfe48a41277f2a764dabbda63fc
RainerMauersberger23 <829532@mystma.org>
2021-02-24 15:55:46 +0000

    Find Doubles

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def findDoubles(w):
     for i in range(0, len(w)):
         if w[i] == w[i + 1]:
             return True
-        else:
-            return False
-print (findDoubles("nintendo"))
+    return False
+
+print (findDoubles("icee"))
