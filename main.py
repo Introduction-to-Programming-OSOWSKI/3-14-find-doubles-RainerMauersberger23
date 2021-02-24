b25e7004c0011a3765908a776a23200a956afa28
RainerMauersberger23 <829532@mystma.org>
2021-02-24 16:06:00 +0000

    Find Doubles Try 2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def findDoubles(w):
 
-    for i in range(0, len(w)):
+    for i in range(0, len(w) - 1):
         if w[i] == w[i + 1]:
             return True
     return False
 
-print (findDoubles("icee"))
+print (findDoubles("jedi"))
